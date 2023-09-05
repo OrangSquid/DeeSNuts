f04abdcc317520f425b45b0847221183589c4eab
Rafael Matos <rafael.dom.matos@gmail.com>
2023-09-05 15:12:40 +0100

    All the data processing commands implemented

--- src/alu.rs
+++ src/alu.rs
@@ -1,12 +1,68 @@
 use crate::arm7::Arm7;
 use crate::arm7::CARRY_FLAG;
+use crate::arm7::OVERFLOW_FLAG;
+use crate::arm7::ZERO_FLAG;
 
 pub trait Alu {
     fn alu_command(&mut self, opcode: u32);
 
-    fn barrel_shifter(&mut self, value: u32, operand: u32, shift_type: u32, register_specified_shift: bool) -> u32;
+    fn barrel_shifter(
+        &mut self,
+        value: u32,
+        operand: u32,
+        shift_type: u32,
+        register_specified_shift: bool,
+    ) -> u32;
 
-    fn decode_alu(&mut self, opcode: u32, set_condition_codes: bool, operand_1: u32, destination_register: u32, operand_2: u32);
+    fn decode_alu(
+        &mut self,
+        opcode: u32,
+        set_condition_codes: bool,
+        operand_1: u32,
+        destination_register: u32,
+        operand_2: u32,
+    );
+
+    fn and(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn exclusive_or(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn subtract(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn right_subtract(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn add(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn add_carry(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn subtract_carry(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn right_subtract_carry(
+        &mut self,
+        operand_1: u32,
+        destination_register: u32,
+        operand_2: u32,
+    ) -> u32;
+
+    fn tst_and(operand_1: u32, operand_2: u32) -> u32;
+
+    fn exclusive_or_teq(operand_1: u32, operand_2: u32) -> u32;
+
+    fn subtract_cmp(operand_1: u32, operand_2: u32) -> u32;
+
+    fn add_cmn(operand_1: u32, operand_2: u32) -> u32;
+
+    fn orr(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn mov(&mut self, destination_register: u32, operand_2: u32) -> u32;
+
+    fn bit_clear(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32;
+
+    fn move_not(&mut self, destination_register: u32, operand_2: u32) -> u32;
+
+    fn set_logical_operations_cpsr_flags(&mut self, result: u32);
+
+    fn set_arithmetic_operations_cpsr_flags(&mut self, operand_1: u32, result: u32, operand_2: u32);
 }
 
 impl Alu for Arm7 {
@@ -39,14 +95,28 @@ impl Alu for Arm7 {
         }
         let alu_opcode = (opcode & 0x1E0_0000) >> 21;
         let set_condition_codes = (opcode & 0x10_0000) == 0x10_0000;
-        self.decode_alu(alu_opcode, set_condition_codes, operand_1, destination_register, operand_2)
+        self.decode_alu(
+            alu_opcode,
+            set_condition_codes,
+            operand_1,
+            destination_register,
+            operand_2,
+        )
     }
 
-    fn barrel_shifter(&mut self, mut value: u32, mut operand: u32, shift_type: u32, register_specified_shift: bool) -> u32 {
+    // TODO make the checks for carry out a seperate functiion
+    // TODO when R15
+    fn barrel_shifter(
+        &mut self,
+        mut value: u32,
+        mut operand: u32,
+        shift_type: u32,
+        register_specified_shift: bool,
+    ) -> u32 {
         match shift_type {
             0x0 => {
                 let carry_bit = (1 as u32) << (32 - value);
-                if operand & carry_bit != 0 {
+                if (operand & carry_bit != 0) && value != 0 {
                     self.cpsr_register |= CARRY_FLAG;
                 }
                 operand << value
@@ -55,8 +125,8 @@ impl Alu for Arm7 {
                 if value == 0 && !register_specified_shift {
                     value = 32;
                 }
-                let carry_bit = (1 as u32) << value - 1;
-                if operand & carry_bit != 0 {
+                let carry_bit = (1 as u32) << (value - 1);
+                if operand & carry_bit != 0 && value != 0 {
                     self.cpsr_register |= CARRY_FLAG;
                 }
                 operand >> value
@@ -65,8 +135,8 @@ impl Alu for Arm7 {
                 if value == 0 && !register_specified_shift {
                     value = 32;
                 }
-                let carry_bit = (1 as u32) << value - 1;
-                if operand & carry_bit != 0 {
+                let carry_bit = (1 as u32) << (value - 1);
+                if operand & carry_bit != 0 && value != 0 {
                     self.cpsr_register |= CARRY_FLAG;
                 }
                 (operand as i32 >> value) as u32
@@ -78,18 +148,169 @@ impl Alu for Arm7 {
                     let carry_in = (self.cpsr_register & CARRY_FLAG) << 2;
                     self.cpsr_register |= carry_bit;
                     operand | carry_in
-                }
-                else {
+                } else {
+                    if value != 0 {
+                        let carry_bit = (1 as u32) << (value - 1);
+                        if operand & carry_bit != 0 {
+                            self.cpsr_register |= CARRY_FLAG;
+                        }
+                    }
                     operand.rotate_right(value)
                 }
             } // RRS
-            _ => panic!()
+            _ => panic!(),
         }
     }
 
-    fn decode_alu(&mut self, opcode: u32, set_condition_codes: bool, operand_1: u32, destination_register: u32, operand_2: u32) {
-        match opcode {
-            _ => ()
+    fn decode_alu(
+        &mut self,
+        opcode: u32,
+        set_condition_codes: bool,
+        operand_1: u32,
+        destination_register: u32,
+        operand_2: u32,
+    ) {
+        let result: u32 = match opcode {
+            0x0 => self.and(operand_1, destination_register, operand_2),
+            0x1 => self.exclusive_or(operand_1, destination_register, operand_2),
+            0x2 => self.subtract(operand_1, destination_register, operand_2),
+            0x3 => self.right_subtract(operand_1, destination_register, operand_2),
+            0x4 => self.add(operand_1, destination_register, operand_2),
+            0x5 => self.add_carry(operand_1, destination_register, operand_2),
+            0x6 => self.subtract_carry(operand_1, destination_register, operand_2),
+            0x7 => self.right_subtract_carry(operand_1, destination_register, operand_2),
+            0x8 => Self::tst_and(operand_1, operand_2),
+            0x9 => Self::exclusive_or_teq(operand_1, operand_2),
+            0xA => Self::subtract_cmp(operand_1, operand_2),
+            0xB => Self::add_cmn(operand_1, operand_2),
+            0xC => self.orr(operand_1, destination_register, operand_2),
+            0xD => self.mov(destination_register, operand_2),
+            0xE => self.bit_clear(operand_1, destination_register, operand_2),
+            0xF => self.move_not(destination_register, operand_2),
+            _ => panic!(),
+        };
+        if set_condition_codes && destination_register != 15 {
+            match opcode {
+                0x0 | 0x1 | 0x8 | 0x9 | 0xC | 0xD | 0xE | 0xF => {
+                    self.set_logical_operations_cpsr_flags(result)
+                }
+                0x2 | 0x3 | 0x4 | 0x5 | 0x6 | 0x7 | 0xA | 0xB => {
+                    self.set_arithmetic_operations_cpsr_flags(operand_1, result, operand_2)
+                }
+                _ => (),
+            }
+        } else if set_condition_codes && destination_register == 15 {
+            self.restore_cpsr();
         }
+    }
+
+    fn set_logical_operations_cpsr_flags(&mut self, result: u32) {
+        self.cpsr_register &= 0xCFFF_FFFF;
+        if result == 0 {
+            self.cpsr_register |= ZERO_FLAG;
+        }
+        self.cpsr_register |= result & 0x8000_0000;
+    }
+
+    fn set_arithmetic_operations_cpsr_flags(
+        &mut self,
+        operand_1: u32,
+        result: u32,
+        operand_2: u32,
+    ) {
+        self.cpsr_register &= 0xFFF_FFFF;
+        self.set_logical_operations_cpsr_flags(result);
+        if (operand_1 | operand_2) & 0x8000_0000 == 0x8000_0000 && result & 0x8000_0000 == 0 {
+            self.cpsr_register |= CARRY_FLAG;
+        }
+        if (operand_1 & operand_2) & 0x8000_0000 == 0 && result & 0x8000_0000 == 0x8000_0000
+            || (operand_1 & operand_2) & 0x8000_0000 == 0x8000_0000 && result & 0x8000_0000 == 0
+        {
+            self.cpsr_register |= OVERFLOW_FLAG;
+        }
+    }
+
+    fn and(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_1 & operand_2;
+        self.registers[destination_register as usize]
+    }
+
+    fn exclusive_or(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_1 ^ operand_2;
+        self.registers[destination_register as usize]
+    }
+
+    fn subtract(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_1.overflowing_sub(operand_2).0;
+        self.registers[destination_register as usize]
+    }
+
+    fn right_subtract(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_2.overflowing_sub(operand_1).0;
+        self.registers[destination_register as usize]
+    }
+
+    fn add(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_2.overflowing_add(operand_1).0;
+        self.registers[destination_register as usize]
+    }
+
+    fn add_carry(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] =
+            operand_1.overflowing_add(operand_2.overflowing_add((self.cpsr_register & CARRY_FLAG) >> 29).0).0;
+        self.registers[destination_register as usize]
+    }
+
+    fn subtract_carry(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] =
+            operand_1.overflowing_sub(operand_2.overflowing_add((self.cpsr_register & CARRY_FLAG) >> 29).0).0.overflowing_sub(1).0;
+        self.registers[destination_register as usize]
+    }
+
+    fn right_subtract_carry(
+        &mut self,
+        operand_1: u32,
+        destination_register: u32,
+        operand_2: u32,
+    ) -> u32 {
+        self.registers[destination_register as usize] =
+        operand_2.overflowing_sub(operand_1.overflowing_add((self.cpsr_register & CARRY_FLAG) >> 29).0).0.overflowing_sub(1).0;
+        self.registers[destination_register as usize]
+    }
+
+    fn tst_and(operand_1: u32, operand_2: u32) -> u32 {
+        operand_1 & operand_2
+    }
+
+    fn exclusive_or_teq(operand_1: u32, operand_2: u32) -> u32 {
+        operand_1 ^ operand_2
+    }
+
+    fn subtract_cmp(operand_1: u32, operand_2: u32) -> u32 {
+        operand_1 - operand_2
+    }
+
+    fn add_cmn(operand_1: u32, operand_2: u32) -> u32 {
+        operand_1 + operand_2
+    }
+
+    fn orr(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_1 & operand_2;
+        self.registers[destination_register as usize]
+    }
+
+    fn mov(&mut self, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_2;
+        self.registers[destination_register as usize]
+    }
+
+    fn bit_clear(&mut self, operand_1: u32, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = operand_1 & !operand_2;
+        self.registers[destination_register as usize]
+    }
+
+    fn move_not(&mut self, destination_register: u32, operand_2: u32) -> u32 {
+        self.registers[destination_register as usize] = !operand_2;
+        self.registers[destination_register as usize]
     }
 }
--- src/arm7.rs
+++ src/arm7.rs
@@ -1,13 +1,13 @@
 use crate::alu::Alu;
 
 // CPU modes
-const USER_MODE: u8 = 0x10;
-const FIQ_MODE: u8 = 0x11;
-const IRQ_MODE: u8 = 0x12;
-const SUPERVISOR_MODE: u8 = 0x13;
-const ABORT_MODE: u8 = 0x17;
-const UNDEFINED_MODE: u8 = 0x1B;
-const SYSTEM_MODE: u8 = 0x1F;
+const USER_MODE: u32 = 0x10;
+const FIQ_MODE: u32 = 0x11;
+const IRQ_MODE: u32 = 0x12;
+const SUPERVISOR_MODE: u32 = 0x13;
+const ABORT_MODE: u32 = 0x17;
+const UNDEFINED_MODE: u32 = 0x1B;
+const SYSTEM_MODE: u32 = 0x1F;
 
 const START_PC: u32 = 0x800_0000;
 
@@ -16,10 +16,10 @@ const STACK_IRQ_START: u32 = 0x300_7FA0;
 const STACK_SUPERVISOR_START: u32 = 0x0300_7FE0;
 
 // Position of the bits in the CPSR register
-const SIGN_FLAG: u32 = 0x8000_0000;
-const ZERO_FLAG: u32 = 0x4000_0000;
+pub const SIGN_FLAG: u32 = 0x8000_0000;
+pub const ZERO_FLAG: u32 = 0x4000_0000;
 pub const CARRY_FLAG: u32 = 0x2000_0000;
-const OVERFLOW_FLAG: u32 = 0x1000_0000;
+pub const OVERFLOW_FLAG: u32 = 0x1000_0000;
 const IRQ_BIT: u32 = 0x80;
 const FIQ_BIT: u32 = 0x40;
 const STATE_BIT: u32 = 0x20;
@@ -38,12 +38,12 @@ pub struct Arm7 {
     supervisor_banked: [u32; 2],
     abort_banked: [u32; 2],
     irq_banked: [u32; 2],
-    undefinied_banked: [u32; 2]
+    undefinied_banked: [u32; 2],
 }
 
 impl Arm7 {
     pub fn new() -> Arm7 {
-        let mut arm7 = Arm7 { 
+        let mut arm7 = Arm7 {
             bios: [0; 0x4000],
             rom: Vec::new(),
             registers: [0; 16],
@@ -54,7 +54,7 @@ impl Arm7 {
             supervisor_banked: [0; 2],
             abort_banked: [0; 2],
             irq_banked: [0; 2],
-            undefinied_banked: [0; 2]
+            undefinied_banked: [0; 2],
         };
         arm7.registers[13] = STACK_USER_SYSTEM_START;
         arm7.irq_banked[0] = STACK_IRQ_START;
@@ -106,55 +106,69 @@ impl Arm7 {
             0x0 => {
                 if opcode & 0x12F_FF10 == 0x12F_FF10 {
                     self.branch_and_exchange(opcode & 0xF);
-                }
-                else {
+                } else {
                     self.alu_command(opcode & 0x3FF_FFFF);
                 }
-            },
+            }
             0x400_0000 => (), // Single Data Transfer or Undefined
             0x800_0000 => match opcode & 0x200_0000 {
                 0x0 => (), // Block Data Transfer
-                0x200_0000 => self.branch(opcode & 0x100_0000 == 0x100_0000, (opcode & 0xFF_FFFF) as i32),
-                _ => ()
-            }
+                0x200_0000 => self.branch(
+                    opcode & 0x100_0000 == 0x100_0000,
+                    (opcode & 0xFF_FFFF) as i32,
+                ),
+                _ => (),
+            },
             0xC00_0000 => match opcode & 0x200_0000 {
                 0x0 => (), // Coprocessor Data Transfer
                 0x200_0000 => match opcode & 0x100_0000 {
-                    0x0 => (), // Coprocessor Data Operation or Register Transfer
+                    0x0 => (),        // Coprocessor Data Operation or Register Transfer
                     0x100_0000 => (), // Software Interrupt
-                    _ => ()
-                }
-                _ => ()
-            }
-            _ => ()
+                    _ => (),
+                },
+                _ => (),
+            },
+            _ => (),
         }
     }
 
     fn check_codition(&mut self, condition: u8) -> bool {
         match condition {
             0x0 => self.cpsr_register & ZERO_FLAG == ZERO_FLAG, // Z flag is set
-            0x1 => self.cpsr_register & ZERO_FLAG == 0x0, // Z flag is not set
+            0x1 => self.cpsr_register & ZERO_FLAG == 0x0,       // Z flag is not set
             0x2 => self.cpsr_register & CARRY_FLAG == CARRY_FLAG, // C flag is set
-            0x3 => self.cpsr_register & CARRY_FLAG == 0x0, // C flag is not set
+            0x3 => self.cpsr_register & CARRY_FLAG == 0x0,      // C flag is not set
             0x4 => self.cpsr_register & SIGN_FLAG == SIGN_FLAG, // S flag is set
-            0x5 => self.cpsr_register & SIGN_FLAG == 0x0, // S flag is not set
+            0x5 => self.cpsr_register & SIGN_FLAG == 0x0,       // S flag is not set
             0x6 => self.cpsr_register & OVERFLOW_FLAG == OVERFLOW_FLAG, // V flag is set
-            0x7 => self.cpsr_register & OVERFLOW_FLAG == 0x0, // V flag is not set
+            0x7 => self.cpsr_register & OVERFLOW_FLAG == 0x0,   // V flag is not set
             0x8 => self.cpsr_register & (CARRY_FLAG | ZERO_FLAG) == CARRY_FLAG, // Unsigned Higher
-            0x9 => self.cpsr_register & CARRY_FLAG == 0 || self.cpsr_register & ZERO_FLAG == ZERO_FLAG, // Unsigned Lower or same
-            0xA => (self.cpsr_register & SIGN_FLAG >> 31) == (self.cpsr_register & OVERFLOW_FLAG >> 28), // Signed Greater than or equal
-            0xB => (self.cpsr_register & SIGN_FLAG >> 31) != (self.cpsr_register & OVERFLOW_FLAG >> 28), // Less than
-            0xC => self.cpsr_register & ZERO_FLAG == 0 && (self.cpsr_register & SIGN_FLAG >> 31) != (self.cpsr_register & OVERFLOW_FLAG >> 28), //
-            0xD => self.cpsr_register & ZERO_FLAG == 0 && (self.cpsr_register & SIGN_FLAG >> 31) == (self.cpsr_register & OVERFLOW_FLAG >> 28),
+            0x9 => {
+                self.cpsr_register & CARRY_FLAG == 0 || self.cpsr_register & ZERO_FLAG == ZERO_FLAG
+            } // Unsigned Lower or same
+            0xA => {
+                (self.cpsr_register & SIGN_FLAG >> 31) == (self.cpsr_register & OVERFLOW_FLAG >> 28)
+            } // Signed Greater than or equal
+            0xB => {
+                (self.cpsr_register & SIGN_FLAG >> 31) != (self.cpsr_register & OVERFLOW_FLAG >> 28)
+            } // Less than
+            0xC => {
+                self.cpsr_register & ZERO_FLAG == 0
+                    && (self.cpsr_register & SIGN_FLAG >> 31)
+                        != (self.cpsr_register & OVERFLOW_FLAG >> 28)
+            } //
+            0xD => {
+                self.cpsr_register & ZERO_FLAG == 0
+                    && (self.cpsr_register & SIGN_FLAG >> 31)
+                        == (self.cpsr_register & OVERFLOW_FLAG >> 28)
+            }
             0xE => true,
             0xF => true,
-            _ => panic!("Condition for opcode is higher than 0xF")
+            _ => panic!("Condition for opcode is higher than 0xF"),
         }
     }
 
-    fn decode_thumb(&mut self, opcode: u16) {
-
-    }
+    fn decode_thumb(&mut self, opcode: u16) {}
 
     fn branch_and_exchange(&mut self, register: u32) {
         let thumb_bit = register & 0x1 << 5;
@@ -172,5 +186,16 @@ impl Arm7 {
         self.registers[15] = temp_pc as u32;
     }
 
-    
+    pub fn restore_cpsr(&mut self) {
+        match self.cpsr_register & 0x1F {
+            USER_MODE => (),
+            FIQ_MODE => self.cpsr_register = self.saved_psr[1],
+            IRQ_MODE => self.cpsr_register = self.saved_psr[2],
+            SUPERVISOR_MODE => self.cpsr_register = self.saved_psr[3],
+            ABORT_MODE => self.cpsr_register = self.saved_psr[4],
+            UNDEFINED_MODE => self.cpsr_register = self.saved_psr[5],
+            SYSTEM_MODE => (),
+            _ => panic!("CPU is an unrecognized mode")
+        }
+    }
 }
